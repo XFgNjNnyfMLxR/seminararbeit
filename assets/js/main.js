591001d0faf58cf13bfa4736bed0bbb1e85144df
seminararbeit <121987763+seminararbeit@users.noreply.github.com>
2023-01-13 22:27:52 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,15 +5,9 @@ document.querySelectorAll('h1[id],h2[id],h3[id],h4[id],h5[id],h6[id]').forEach(e
 i=document.getElementById('inner')
 
 if(i!==null){
+  i.addEventListener('swiped-down',e=>{if(e.detail.yStart<100){location="https://seminararbeit.github.io"}})
   l=document.getElementById('left-button')
   r=document.getElementById('right-button')
-  
-  i.addEventListener('swiped-down',e=>{
-    console.log(e.detail.yStart)
-    if(e.detail.yStart<100) {
-      location="https://seminararbeit.github.io"
-    }
-  })
   
   if(l!=null){
     i.addEventListener('swiped-left',_=>{location.href=l.href})
